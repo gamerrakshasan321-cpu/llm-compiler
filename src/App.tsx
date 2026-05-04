458eb7c0770d8c9e90b4890d529513e658a6a108
Akhilus26 <akhilus321@gmail.com>
2026-05-04 20:45:05 +0530

    Set BrowserRouter basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/llm-compiler">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/workspace" element={<WorkspacePage />} />
